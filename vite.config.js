660256e0617f863c11573cec971de4fc0aa4f2d1
tiamangea <tiamangea01@gmail.com>
2026-05-15 18:35:20 +0700

    Fix: Update base vite dan deploy via folder docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Baris base ini yang penting untuk GitHub Pages
+  base: '/ecocraft-app/', 
+  
   plugins: [react()],
 })
