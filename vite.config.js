6a53df1f7a7dcd44864f2db5ef1f7897cf74bf74
tiamangea <tiamangea01@gmail.com>
2026-05-15 23:14:46 +0700

    Fix: Hapus path lama dan perbarui base URL

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Ubah base menjadi root (garis miring saja) karena sudah pakai domain sendiri
-  base: '/', 
-  
+  base: '/', // Pastikan hanya garis miring ini saja
   plugins: [react()],
 })
