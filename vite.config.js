9cb55b349ec37c6761ea7166028f229b4cb4ffe9
tiamangea <tiamangea01@gmail.com>
2026-05-15 18:39:25 +0700

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Baris base ini yang penting untuk GitHub Pages
-  base: '/ecocraft-app/', 
+  // Saya telah mengubahnya menyesuaikan dengan nama asli repositori Anda di GitHub
+  base: '/EcoCraft.id/', 
   
   plugins: [react()],
 })
