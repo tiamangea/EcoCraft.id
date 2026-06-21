508177067b00fea77206cd7c2b095e13de315e11
tiamangea <tiamangea01@gmail.com>
2026-06-21 20:46:52 +0700

    Fix configuration typo and update docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // Perbaikan di baris ini
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Pastikan hanya garis miring ini saja
   plugins: [react()],
+  base: '/EcoCraft.id/',
 })
