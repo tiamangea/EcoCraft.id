d7a786f29987616407dbb615314458e64b697614
tiamangea <tiamangea01@gmail.com>
2026-06-21 20:57:14 +0700

    Deploy: change build output to docs folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Perbaikan di baris ini
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EcoCraft.id/',
+  base: '/EcoCraft.id/', // Sudah disesuaikan dengan nama repositori kamu
+  build: {
+    outDir: 'docs', // Mengatur agar hasil build langsung masuk ke folder docs
+  },
 })
