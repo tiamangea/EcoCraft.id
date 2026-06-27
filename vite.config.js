a4e14ceabc9b32081aeaa7dd7b0a4eb8209ae4ee
tiamangea <tiamangea01@gmail.com>
2026-06-27 23:57:54 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // Perbaikan di baris ini
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EcoCraft.id/', // Sudah disesuaikan dengan nama repositori kamu
-  build: {
-    outDir: 'docs', // Mengatur agar hasil build langsung masuk ke folder docs
-  },
+  base: '/EcoCraft.id/',
 })
